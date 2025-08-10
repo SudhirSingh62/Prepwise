ebae75f658794be912345e46918d1279d9bc4edd
Sudhir Singh <sudhirsingh@Sudhirs-MacBook-Air.local>
2025-08-11 01:01:13 +0530

    Setup app, file and folder structure, routing and auth pages

---
+++ app/(auth)/sign-up/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Page = () => {
+    return (
+        <div>Page</div>
+    )
+}
+export default Page
+import AuthForm from "@/components/AuthForm";
+
+const Page = () => {
+    return <AuthForm type="sign-up" />;
+};
+
+export default Page;
