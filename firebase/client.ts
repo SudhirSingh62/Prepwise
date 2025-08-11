31812c5d929b918fec3dad9794cfca578f053fe3
Sudhir Singh <sudhirsingh@Sudhirs-MacBook-Air.local>
2025-08-11 19:21:44 +0530

    Deployment on Vercel-2

--- firebase/client.ts
+++ firebase/client.ts
@@ -1,29 +1,4 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-    apiKey: "AIzaSyBCquZ7I1VcxbR8xFe8ZcGg76qKUYspALI",
-    authDomain: "prepwise-cd5a9.firebaseapp.com",
-    projectId: "prepwise-cd5a9",
-    storageBucket: "prepwise-cd5a9.firebasestorage.app",
-    messagingSenderId: "438868268511",
-    appId: "1:438868268511:web:599979161bf126a726e06a",
-    measurementId: "G-3ZFD3YFSL7"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-
-
-
-
-// Import the functions you need from the SDKs you need
 import { getApp, getApps, initializeApp } from "firebase/app";
 // import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
